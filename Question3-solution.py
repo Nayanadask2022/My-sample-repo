1371d89f0988f2657b83803b6d3f5fdfd5f2df1a
Nayanadask2022 <102152977+Nayanadask2022@users.noreply.github.com>
2024-05-22 20:48:43 +0530

    Create Question3-solution.py
    This is a python file

---
+++ Question3-solution.py
@@ -0,0 +1,11 @@
+import re #Importing re module for regular expression
+input_string = "$180,240/y"
+
+# printing original string 
+print("The original string : " + input_string)
+
+#Importing regular expression
+output_int = re.sub("\D", "", input_string)
+
+# print result
+print("The numbers list is :" + str(output_int))
